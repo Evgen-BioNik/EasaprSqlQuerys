ba6a1006dd1ef21b44373c49ecd9c65aa1f6ffdf
Evgen.ServHell <leo.jeka.work@gmail.com>
2021-08-20 02:00:02 +0300

    new day - new commit

---
+++ _АРЛ/SQLQuery.sql
@@ -0,0 +1,8 @@
+UPDATE nsi.dbo.t_aofType
+SET		[active] = 0,
+		forNsi = null,
+		sendEtran = 0
+WHERE id IN (426, 434, 620, 732, 733, 734, 832, 931, 772, 1136, 1137, 1142, 1040, 605, 606, 616, 663, 842, 866)
+
+
+
