193e3ac6a9dd6fd77dbd5373a0570aa61ece74ac
Evgen.ServHell <leo.jeka.work@gmail.com>
2021-08-27 02:00:02 +0300

    new day - new commit

--- _АРЛ/АРЛ. OverLoad. Анализ отсутствия акта для сверхнорматива.sql
+++ _АРЛ/АРЛ. OverLoad. Анализ отсутствия акта для сверхнорматива.sql
@@ -1,44 +1,47 @@
 
 -- первый норматив, от прибытия до подачи
-select dateArrive, dateArrive, dateFeed, controlFeedDate,
-	datediff(mi,dateArrive, dateFeed)/60.0 as factDiff,
-	datediff(mi,dateArrive, dateFeed)/60.0 as controlDiff,
+SELECT id, dateArrive, dateArrive, dateFeed, controlFeedDate,
+	datediff(mi,dateArrive, dateFeed)/60.0 AS factDiff,
+	datediff(mi,dateArrive, dateFeed)/60.0 AS controlDiff,
 	normFeedOper, factFeedOper,
 	datediff(mi,dateArrive, datedep)/60.0 cap,
-	datediff(mi,dateArrive, datedep)/60.0 - 24 as overcap,
+	datediff(mi,dateArrive, datedep)/60.0 - 24 AS overcap,
 	
 	controlFeed, aktFeed, aofidFeed,
 	
- '' as t1,'' t2, sendNumArrive, m.carnum, privateCar, BreakStone, innerdelivery, InsideShipping
-from vag.dbo.modelArrive as m 
-	left join vag.dbo.modelArrive_violation as mv 
-		on mv.idModelArrive = m.id 
-where sendnumarrive = 'ЭР059461'  and m.carnum = '63109433'
+	'' AS t1,'' t2, m.sendnumArrive, m.carnum, privateCar, BreakStone, innerdelivery, InsideShipping
+FROM vag.dbo.modelArrive AS m 
+	LEFT JOIN vag.dbo.modelArrive_violation AS mv
+		ON mv.idModelArrive = m.id 
+WHERE 1=1
+	AND m.carnum IN ( '62206529' )
+	AND m.sendnumArrive IN ( 'ЭР059461' )
+ORDER BY dateArrive DESC
 
 
 -- второй норматив, от подачи до окончания грузовой операции
-SELECT id,dateArrive, DATEDIFF(d, dateArrive, '2021-03-29 13:08:52.380') AS diff,
+SELECT id, dateArrive, DATEDIFF(d, dateArrive, '2021-03-29 13:08:52.380') AS diff,
 	dateFeed, controlFeeddate, dateGruz, controlGruzDate,
-	datediff(mi,dateFeed, dateGruz)/60.0 as factDiff,
-	datediff(mi,controlFeeddate, dateGruz)/60.0 as controlDiff,
+	datediff(mi,dateFeed, dateGruz)/60.0 AS factDiff,
+	datediff(mi,controlFeeddate, dateGruz)/60.0 AS controlDiff,
 	normGruzOper, factGruzOper,
 	datediff(mi,dateArrive, datedep)/60.0 cap,
-	datediff(mi,dateArrive, datedep)/60.0 - 24 as overcap,
+	datediff(mi,dateArrive, datedep)/60.0 - 24 AS overcap,
 	
 	controlGruz, aktGruz, aofidGruz,
 	
- '' AS t1,'' t2, sendNumArrive, m.carnum, privateCar, BreakStone, innerdelivery, InsideShipping
+	'' AS t1,'' t2, m.sendnumArrive, m.carnum, privateCar, BreakStone, innerdelivery, InsideShipping
 FROM vag.dbo.modelArrive AS m 
-	LEFT JOIN vag.dbo.modelArrive_violation AS mv 
-		ON mv.idModelArrive = m.id 
+	LEFT JOIN vag.dbo.modelArrive_violation AS mv
+		ON mv.idModelArrive = m.id
 WHERE 1=1
 	AND m.carnum IN ( '52990330' )
-	AND sendnumarrive IN ( 'ЭЧ213300' )
+	AND m.sendnumArrive IN ( 'ЭЧ213300' )
 ORDER BY dateArrive DESC
 
 
 -- третий норматив, от окончания грузовой операции до уборки
-SELECT dateArrive, DATEDIFF(d, dateArrive, '2021-03-19 13:08:52.380') AS diff,
+SELECT id, dateArrive, DATEDIFF(d, dateArrive, '2021-03-19 13:08:52.380') AS diff,
 	dateGruz, controlGruzdate, dateClear, controlClearDate,
 	datediff(mi,dateGruz, dateClear)/60.0 AS factDiff,
 	datediff(mi,controlGruzdate, dateClear)/60.0 AS controlDiff,
@@ -48,28 +51,31 @@ SELECT dateArrive, DATEDIFF(d, dateArrive, '2021-03-19 13:08:52.380') AS diff,
 	
 	controlClear, aktClear, aofidClear,
 	
- '' AS t1,'' t2, sendNumArrive, m.carnum, privateCar, BreakStone, innerdelivery, InsideShipping
+	'' AS t1,'' t2, m.sendnumArrive, m.carnum, privateCar, BreakStone, innerdelivery, InsideShipping
 FROM vag.dbo.modelArrive AS m 
-	LEFT JOIN vag.dbo.modelArrive_violation AS mv 
-		ON mv.idModelArrive = m.id 
+	LEFT JOIN vag.dbo.modelArrive_violation AS mv
+		ON mv.idModelArrive = m.id
 WHERE 1=1
 	AND m.carnum IN ( '62206529' )
-	AND sendnumarrive IN ( 'ЭР059461' )
+	AND m.sendnumArrive IN ( 'ЭР059461' )
 ORDER BY dateArrive DESC
 
 
 -- четвертый норматив, от уборки до отправления
-select  dateClear, controlCleardate, dateDep, controlDepDate,
-	datediff(mi,dateClear, dateDep)/60.0 as factDiff,
-	datediff(mi,controlCleardate, dateDep)/60.0 as controlDiff,
+SELECT id, dateClear, controlCleardate, dateDep, controlDepDate,
+	datediff(mi,dateClear, dateDep)/60.0 AS factDiff,
+	datediff(mi,controlCleardate, dateDep)/60.0 AS controlDiff,
 	normDepOper, factDepOper,
 	datediff(mi,dateArrive, datedep)/60.0 cap,
-	datediff(mi,dateArrive, datedep)/60.0 - 24 as overcap,
+	datediff(mi,dateArrive, datedep)/60.0 - 24 AS overcap,
 	
 	controlDep, aktDep, aofidDep,
 	
- '' as t1,'' t2, sendNumArrive, m.carnum, privateCar, BreakStone, innerdelivery, InsideShipping
-from vag.dbo.modelArrive as m 
-	left join vag.dbo.modelArrive_violation as mv 
-		on mv.idModelArrive = m.id 
-where sendnumarrive = 'ЭР059461'  and m.carnum = '30508386'
+	'' AS t1,'' t2, m.sendnumArrive, m.carnum, privateCar, BreakStone, innerdelivery, InsideShipping
+FROM vag.dbo.modelArrive AS m 
+	LEFT jOin vag.dbo.modelArrive_violation AS mv
+		ON mv.idModelArrive = m.id
+WHERE 1=1
+	AND m.carnum IN ( '62206529' )
+	AND m.sendnumArrive IN ( 'ЭР059461' )
+ORDER BY dateArrive DESC
