3e736d27d5335fa1a6c3f8fc350b922414832ffe
Evgen.ServHell <leo.jeka.work@gmail.com>
2022-02-25 02:00:01 +0300

    new day - new commit

--- _Остальное/Форум. Заткнуть раз и навсегда.sql
+++ _Остальное/Форум. Заткнуть раз и навсегда.sql
@@ -1,3 +1,5 @@
+-- Ебашить на 43й.
+
 DECLARE @text varchar(255) = 'Здравствуйте. В связи с переходом на авторскую поддержку проектов ЕАСАПР на ЕСПП (Единую службу поддержки пользователей) просим все обращения формировать через неё. В обращении указывайте систему, в которой работаете, и логин для доступа в нее.'
 
 
@@ -5,8 +7,8 @@ UPDATE TOP (1000) admin.dbo.forum
 SET MsgSolvingStatus = 1
 	OUTPUT INSERTED.MsgNumRec, 2467, GETDATE(), 1, 1, @text
 	INTO admin.dbo.forum (ParentId, MsgAuthor, MsgDate, Subsystem, MsgSolvingStatus, MsgText )
-where Subsystem = 1 
-	and MsgSolvingStatus is null
-	and MsgDate > '2020'
-	and ParentId = 0
-	and MsgStatus is null
+WHERE Subsystem = 1 -- Система где затыкать. 1 - АРЛ.
+	AND MsgSolvingStatus is null
+	AND MsgDate > '2021'
+	AND ParentId = 0
+	AND MsgStatus is null
